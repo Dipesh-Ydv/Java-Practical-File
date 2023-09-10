cc7466a259148d91bb7eb39a9292881fc9d565e7
Dipesh Yadav <dipeshyadav4444@gmail.com>
2023-09-10 09:35:48 +0530

    program 28 added

---
+++ src/com/dipesh/Pr28.java
@@ -0,0 +1,16 @@
+package com.dipesh;
+
+import java.awt.*;
+
+public class Pr28 {
+    public static void main(String[] args) {
+        // creating instance of Frame class
+        Frame fr = new Frame();
+        // setting the title for the Frame
+        fr.setTitle("Frame Window");
+        // setting size of the frame
+        fr.setSize(500, 400);
+        // setting the frame visible
+        fr.setVisible(true);
+    }
+}
