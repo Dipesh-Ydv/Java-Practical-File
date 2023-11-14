03811a6b9fc59339eef0b7755b6261b78c34dbca
Dipesh Yadav <dipeshyadav4444@gmail.com>
2023-11-14 14:46:54 +0530

    Program 40 added

--- src/com/dipesh/Pr40.java
+++ src/com/dipesh/Pr40.java
@@ -2,79 +2,76 @@ package com.dipesh;
 
 public class Pr40 {
     public static void main(String[] args) {
+        MyValue value = new MyValue();
 
-        Data data = new Data();
+        Producer p = new Producer(value);
+        Consumer c = new Consumer(value);
 
-        Producer producer = new Producer(data);
-        Consumer consumer = new Consumer(data);
-
-        // creating threads
-        Thread t1 = new Thread(producer);
-        Thread t2 = new Thread(consumer);
-
-        // starting threads
-        t1.start();
-        t2.start();
-
+        p.start();
+        c.start();
     }
 }
 
-class Producer implements Runnable {
+class MyValue {
+    int value = 0;
+    boolean flag = true;
 
-    private final Data data;
-
-    public Producer(Data data) {
-        this.data = data;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            synchronized (data) {
-                System.out.println("Producing : " + i);
-                data.setValue(i);
-                data.notify(); // Notify the Consumer Thread
+    synchronized public void setValue(int value) {
+        while (flag != true) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
             }
         }
-    }
-}
-
-class Consumer implements Runnable {
-
-    private final Data data;
-
-    public Consumer(Data data) {
-        this.data = data;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            synchronized (data) {
-                System.out.println("Waiting for Data...");
-                if (data.getValue() == 0) {
-                    try {
-                        data.wait(); // Put the Consumer Thread to Wait
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println("Consuming : " + data.getValue());
-                data.notify(); // Notify the Producer Thread
-            }
-        }
-    }
-}
-
-class Data {
-
-    private int value;
-
-    public synchronized void setValue(int value) {
         this.value = value;
+        flag = false;
+        notify();
     }
 
-    public synchronized int getValue() {
-        return this.value;
+    synchronized public int getValue() {
+        while (flag != false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        int x = this.value;
+        flag = true;
+        notify();
+        return x;
+    }
+}
+
+class Producer extends Thread {
+    MyValue v;
+    Producer (MyValue data) {
+        v = data;
+    }
+
+    @Override
+    public void run() {
+        int i = 1;
+        while (true) {
+            v.setValue(i);
+            System.out.println("Producer: " + i);
+            i++;
+        }
+    }
+}
+
+class Consumer extends Thread {
+    MyValue v;
+
+    Consumer(MyValue data) {
+        v = data;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("Consumer: " + v.getValue());
+        }
     }
 }
