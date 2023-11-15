2c03674da0d90e638ab8a1d271504cf05c1899e3
Dipesh Yadav <dipeshyadav4444@gmail.com>
2023-11-15 19:25:24 +0530

    Program 25 added

--- src/com/dipesh/Pr25.java
+++ src/com/dipesh/Pr25.java
@@ -1,2 +1,56 @@
-package com.dipesh;public class Pr25 {
+package com.dipesh;
+
+public class Pr25 {
+    public static void main(String[] args) {
+        Employee employee = new Employee(1, "Ramphal", "Developer", 696969);
+        SalaryCalculator salaryCalculator = new SalaryCalculator();
+
+        int calculatedSalary = salaryCalculator.calculateSalary(employee);
+        System.out.println("The calculated salary is: " + calculatedSalary);
+    }
+}
+
+class Employee {
+    private int id;
+    private String name;
+    private String role;
+    private int salary;
+
+    public Employee(int id, String name, String role, int salary) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+
+interface CalculateSalary {
+    int calculateSalary(Employee employee);
+}
+
+class SalaryCalculator implements CalculateSalary {
+    @Override
+    public int calculateSalary(Employee employee) {
+        return switch (employee.getRole()) {
+            case "Developer" -> employee.getSalary() * 12;
+            case "Manager" -> employee.getSalary() * 14;
+            default -> 0;
+        };
+    }
 }
